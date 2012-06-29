5368eb462176098a44a354b80595e07e1f8c3dd0
Lucian Mogoșanu <lucian.mogosanu@gmail.com>
2012-06-29 22:16:38 +0300

    Markov: Chain: obtain accessible states

--- Markov/Chain.hs
+++ Markov/Chain.hs
@@ -10,3 +10,10 @@ type Chain a = M.Map a [(a, Float)]
 -- build a Markov chain from a key-value list
 fromList :: Ord a => [(a, [(a, Float)])] -> Chain a
 fromList = M.fromList
+
+-- get the possible state-probability pairs from
+-- of a given state
+accessibleStates :: Ord a => Chain a -> a -> [(a, Float)]
+accessibleStates c s = case lookup s c of
+    Just accs -> accs
+    Nothing -> []
