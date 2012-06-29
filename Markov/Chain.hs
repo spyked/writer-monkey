e581304853c68cc32db790a970dfcfdfaebcffb4
Lucian Mogoșanu <lucian.mogosanu@gmail.com>
2012-06-29 23:01:28 +0300

    Markov: Chain: random walks through chain

--- Markov/Chain.hs
+++ Markov/Chain.hs
@@ -1,6 +1,8 @@
 module Markov.Chain where
 
 import Data.Map as M
+import System.Random (randomRIO)
+import Data.List (sortBy)
 
 -- Markov Chain: DAG with probabilities on arcs
 -- also, a dictionary that associates a
@@ -17,3 +19,34 @@ accessibleStates :: Ord a => Chain a -> a -> [(a, Float)]
 accessibleStates c s = case M.lookup s c of
     Just accs -> accs
     Nothing -> []
+
+-- sort a list of state-probability pairs by probabilities
+sortByProb :: [(a, Float)] -> [(a, Float)]
+sortByProb = sortBy (.<.)
+    where
+    sp .<. sp' = snd sp' `compare` snd sp
+
+-- given a state, randomly select a next state from a chain
+-- based on the probability distribution of the accessible
+-- states
+next :: Ord a => Chain a -> a -> IO a
+next c s = do
+    rn <- randomRIO (0, 1)
+    return $ next' rn 0 $ sortByProb $ accessibleStates c s
+    where
+    next' _ _ [] = error "No states available."
+    next' _ _ (sp : []) = fst sp
+    next' rn acc ((s, p) : sps) = -- sample
+        if rn <= acc + p
+            then s
+            else next' rn (acc + p) sps
+
+-- take a random walk of a given number of steps
+-- through a given Markov process
+randomWalk :: Ord a => Chain a -> a -> Int -> IO [a]
+randomWalk c s n = if n <= 0
+    then return []
+    else do
+        ns <- next c s
+        rw' <- randomWalk c ns (n - 1)
+        return $ s : rw'
