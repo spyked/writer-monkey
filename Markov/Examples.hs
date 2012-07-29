06d2dd060a321fdff4f34ad2b1b862923d676bd0
Lucian Mogoșanu <lucian.mogosanu@gmail.com>
2012-07-29 20:19:54 +0300

    Markov: Examples: drunkard

--- Markov/Examples.hs
+++ Markov/Examples.hs
@@ -11,3 +11,17 @@ weatherChain = fromList [
     (Sunny, [(Sunny, 0.6), (Rainy, 0.4)]),
     (Rainy, [(Sunny, 0.7), (Rainy, 0.3)])
     ]
+
+-- "drunkard" random walk
+data Direction = L | R deriving (Eq, Ord, Show)
+
+drunkardChain :: Chain Direction
+drunkardChain = fromList [
+    (L, [(L, 0.5), (R, 0.5)]),
+    (R, [(L, 0.5), (R, 0.5)])
+    ]
+
+count :: [Direction] -> Int
+count [] = 0
+count (L : rest) = 1 + count rest
+count (R : rest) = -1 + count rest
