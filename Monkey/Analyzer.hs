285273d84e966f0fc1cf9e4cecf366ca94326c7e
Lucian Mogosanu <lucian.mogosanu@gmail.com>
2012-11-25 20:16:17 +0200

    Monkey: Analyzer: implement analysis

--- Monkey/Analyzer.hs
+++ Monkey/Analyzer.hs
@@ -2,10 +2,27 @@ module Monkey.Analyzer (analyze) where
 
 -- text analysis resulting in a Markov model
 
+import Monkey.Util.Processing
 import Markov.Chain (Chain, fromList)
 import Data.List (nub)
 
--- we assume that a "text" is actually a sequence of words, each word
--- being a token for the analyzer.
-analyze :: [String] -> Chain String
-analyze = undefined
+-- we assume that a "text" is actually a sequence of tokens
+analyze :: (Eq a, Ord a) => [a] -> Chain a
+analyze text = fromList $ zip uniqs $ map doAnalyze uniqs
+    where
+    uniqs = nub text
+    doAnalyze = flip analyzeToken $ text
+
+-- given a token and its context, construct a follower-probability
+-- model
+analyzeToken :: Eq a => a -> [a] -> [(a, Float)]
+analyzeToken x = normalizeFollowers x . occurenceList . consecutivesOf x
+
+-- convert integers into probabilities
+-- by default, states without followers loop into themselves (first equation)
+normalizeFollowers :: Eq a => a -> [(a, Int)] -> [(a, Float)]
+normalizeFollowers x [] = [(x,1)]
+normalizeFollowers _ fs = map (./. total) fs
+    where
+    total = fromIntegral . sum $ map snd fs
+    (x, occ) ./. n = (x, fromIntegral occ / n)
