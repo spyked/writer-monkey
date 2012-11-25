c1c7b1ab11c24ea3da7db689bb0e2adfde7a5b7b
Lucian Mogosanu <lucian.mogosanu@gmail.com>
2012-11-25 18:36:21 +0200

    Monkey.Util: add func to convert punctuation into whitespace

--- Monkey/Util.hs
+++ Monkey/Util.hs
@@ -1,11 +1,19 @@
 module Monkey.Util where
 
+import qualified Data.Char as C (toLower)
+
 -- various utility functions
 punctuation :: String
-punctuation = ",.;?!'-[](){}"
+punctuation = ",./;\\[]<>?:\"|{}`~!@#$%^&*()-_=+"
 
 notContains :: Eq a => [a] -> a -> Bool
 notContains = flip notElem
 
 stripPunctuation :: String -> String
 stripPunctuation = filter $ notContains punctuation
+
+wsPunctuation :: String -> String
+wsPunctuation = map $ \ c -> if elem c punctuation then ' ' else c
+
+toLower :: String -> String
+toLower = map C.toLower
