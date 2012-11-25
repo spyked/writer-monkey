b39159b446df3248955430fafc6f9861ad5c93d7
Lucian Mogosanu <lucian.mogosanu@gmail.com>
2012-11-25 19:40:26 +0200

    Monkey: Analyzer: export analyze function

--- Monkey/Analyzer.hs
+++ Monkey/Analyzer.hs
@@ -1,3 +1,11 @@
-module Monkey.Analyzer where
+module Monkey.Analyzer (analyze) where
 
 -- text analysis resulting in a Markov model
+
+import Markov.Chain (Chain, fromList)
+import Data.List (nub)
+
+-- we assume that a "text" is actually a sequence of words, each word
+-- being a token for the analyzer.
+analyze :: [String] -> Chain String
+analyze = undefined
