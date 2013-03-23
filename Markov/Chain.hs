4f32cfff839024e13e5b22a3b841371dba16afc3
Lucian Mogosanu <lucian.mogosanu@gmail.com>
2013-03-23 23:48:49 +0200

    Markov: Chain: stop when no states are accessible
    Previously, an error was thrown when no accessible states were available. Now
    we don't guarantee that walks last the given number of steps, but we don't fail
    in that ugly manner.

--- Markov/Chain.hs
+++ Markov/Chain.hs
@@ -36,25 +36,29 @@ sumProb = sum . map snd
 -- given a state, randomly select a next state from a chain
 -- based on the probability distribution of the accessible
 -- states
-next :: Ord a => Chain a -> a -> IO a
+next :: Ord a => Chain a -> a -> IO (Maybe a)
 next c s = do
     let accs = accessibleStates c s
     rn <- randomRIO (0, sumProb accs)
     return $ next' rn 0 $ sortByProb accs
     where
-    next' _ _ [] = error "No states available."
-    next' _ _ (sp : []) = fst sp
+    next' _ _ [] = Nothing
+    next' _ _ (sp : []) = Just $ fst sp
     next' rn acc ((s, p) : sps) = -- sample
         if rn <= acc + p
-            then s
+            then Just s
             else next' rn (acc + p) sps
 
--- take a random walk of a given number of steps
+-- take a random walk of a maximum given number of steps
 -- through a given Markov process
+-- TODO: refactor ugly code
 randomWalk :: Ord a => Chain a -> a -> Int -> IO [a]
 randomWalk c s n = if n <= 0
     then return []
     else do
-        ns <- next c s
-        rw' <- randomWalk c ns (n - 1)
-        return $ s : rw'
+        mns <- next c s
+        case mns of
+            Nothing -> return []
+            Just ns -> do
+                rw' <- randomWalk c ns (n - 1)
+                return $ s : rw'
