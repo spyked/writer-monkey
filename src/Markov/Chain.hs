00255cabe60ed6bbcb07340797949e7551f089aa
Lucian Mogosanu <lucian.mogosanu@gmail.com>
2013-03-24 14:43:25 +0200

    Markov: Chain: use maps for accessible states

--- src/Markov/Chain.hs
+++ src/Markov/Chain.hs
@@ -3,24 +3,30 @@ module Markov.Chain where
 import qualified Data.Map as M
 import System.Random (randomRIO)
 import Data.List (sortBy)
+import Control.Arrow (second)
 
 -- Markov Chain: DAG with probabilities on arcs
 -- also, a dictionary that associates a
 -- probability to each successor
-type Chain a = M.Map a [(a, Int)]
+type Distribution a = M.Map a Int
+newtype Chain a = Chain { unChain :: M.Map a (Distribution a) }
+                deriving (Eq, Ord, Show)
 
 -- build a Markov chain from a key-value list
 fromList :: Ord a => [(a, [(a, Int)])] -> Chain a
-fromList = M.fromList
+fromList = Chain . M.fromList . map (second M.fromList)
 
 states :: Chain a -> [a]
-states = M.keys
+states = M.keys . unChain
+
+distributions :: Chain a -> [Distribution a]
+distributions = M.elems . unChain
 
 -- get the possible state-probability pairs from
 -- of a given state
 accessibleStates :: Ord a => Chain a -> a -> [(a, Int)]
-accessibleStates c s = case M.lookup s c of
-    Just accs -> accs
+accessibleStates c s = case M.lookup s (unChain c) of
+    Just accs -> M.toList accs -- TODO: use Distribution
     Nothing -> []
 
 -- sort a list of state-probability pairs by probabilities
